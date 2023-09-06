ec51dd9fd806e12eb1b328d9ec886071449e0ecf
niquola <niquola@gmail.com>
2023-09-06 16:37:58 -0700

    rm linting noise

--- tests/lint.js
+++ tests/lint.js
@@ -3,11 +3,12 @@ const ajv = new Ajv({allErrors: true});
 
 var schema = require('./tests.schema.json');
 const validate = ajv.compile(schema);
-console.log(schema);
 
 const path = require('path');
 const fs = require('fs');
 const directoryPath = path.join(__dirname, 'v1');
+
+console.log('Linting tests...');
 fs.readdir(directoryPath, function (err, files) {
     if (err) {
         return console.log('Unable to scan directory: ' + err);
