7f50f3a53cf51caebac6b0d470627be48f003bcc
niquola <niquola@gmail.com>
2023-09-09 12:13:54 -0700

    remove old sh

--- build_tests.sh
+++
@@ -1,8 +0,0 @@
-rm -rf input/tests
-mkdir -p input/tests
-for f in tests/*.jsonnet
-do
- r=$(echo "$f" | sed "s/jsonnet/json/" | sed "s/tests\//input\/tests\//")
- echo "* $f > $r"
- jsonnet $f >  $r
-done
