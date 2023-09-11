f2e0a877eeb0deb461f67f4a6ec623b84d0f9388
niquola <niquola@gmail.com>
2023-09-11 14:37:54 -0700

    render tests preview

--- test_report/src/App.svelte
+++ test_report/src/App.svelte
@@ -59,33 +59,49 @@
             {/each}
         </nav>
         <main id="content" class="flex-1 min-w-0 overflow-auto px-8 py-4">
-            <h1 class="text-3xl">{current?.title}</h1>
-            <p>{current?.description || ''}</p>
-            <b>Tests</b>
+            <h1 class="text-3xl border-b mt-3 mb-1">{current?.title}</h1>
+            <p class="my-2 text-gray-600">{current?.description || ''}</p>
+            <h2 class="mt-4 mb-2 text-sm font-bold text-gray-500">Dataset</h2>
+            <div class="border divide-y">
+                {#each current?.resources || [] as resource}
+                    <details>
+                        <summary class="px-4 py-1 hover:bg-gray-100 cursor-pointer">
+                            {resource.resourceType} / <b>{resource.id}</b>
+                            <span class="text-xs text-gray-500"> {JSON.stringify(resource).substring(0,160)}... </span>
+                        </summary>
+                        <pre class="border-t text-xs bg-gray-100 p-4">{ JSON.stringify(resource,false, " ")}</pre>
+                    </details>
+                {/each}
+            </div>
+            <h2 class="mt-6 mb-2 text-sm font-bold text-gray-500">Tests</h2>
             {#each current?.tests || [] as test}
                 <div class="my-4">
                     <h2 class="text-2xl my-3 border-b">{test.title}</h2>
-                    <p>{test.description || ''}</p>
-                    <pre class="my-2 text-sm p-2 border rounded bg-gray-100">{JSON.stringify(test.view,null,2) }</pre>
-                    <h3 class="text-lg">Expected:</h3>
-                    <table>
-                        <thead>
-                            <tr>
-                                {#each columns(test.expect) || [] as col}
-                                    <th class="px-2 py-1 bg-gray-100 border">{col}</th>
-                                {/each}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {#each test.expect || [] as row}
+                    <p class="my-2 text-gray-600">{test?.description || ''}</p>
+                    <div class="flex space-x-4 items-start">
+                        <pre class="flex-1 text-xs p-2 border rounded bg-gray-100">{JSON.stringify(test.view,null,2) }</pre>
+                        <table class="flex-1">
+                            {#if test.expectError}
+                                <div class="text-red-500 p-4"> Expected error</div>
+                            {/if }
+                            <thead>
                                 <tr>
                                     {#each columns(test.expect) || [] as col}
-                                        <td class="px-2 py-1 border">{pp(row[col])}</td>
+                                        <th class="px-2 py-1 bg-gray-100 border">{col}</th>
                                     {/each}
                                 </tr>
-                            {/each}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {#each test.expect || [] as row}
+                                    <tr>
+                                        {#each columns(test.expect) || [] as col}
+                                            <td class="px-2 py-1 border">{pp(row[col])}</td>
+                                        {/each}
+                                    </tr>
+                                {/each}
+                            </tbody>
+                        </table>
+                    </div>
 
                 </div>
             {/each}
