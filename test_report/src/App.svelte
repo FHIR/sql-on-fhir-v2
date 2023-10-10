7a6b663f89d756d37a675e8dfc5d95377a73dd32
niquola <niquola@gmail.com>
2023-10-10 11:56:39 +0100

    fix logo

--- test_report/src/App.svelte
+++ test_report/src/App.svelte
@@ -27,7 +27,7 @@
 <div>
     <div class="flex space-x-4 px-4 py-2 border-b items-center" style="background-color: rgb(239 253 254);">
         <a href="" class="flex items-center px-4 text-lg"> 
-            <img src="/logo.jpeg" class="w-12 h-12" style="" /> SQL on FHIR 2.0
+            <img src="logo.jpeg" class="w-12 h-12" style="" /> SQL on FHIR 2.0
         </a>
         <a href="#pg" class="px-2 hover:text-blue-500 hover:bg-gray-150">Playground</a>
         <a href="#" class="px-2 hover:text-blue-500 hover:bg-gray-150">Tests</a>
