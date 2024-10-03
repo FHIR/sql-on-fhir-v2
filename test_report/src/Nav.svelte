cb9107c0159ed1d700ee84969b8701cadfd2cb86
niquola <niquola@gmail.com>
2024-10-03 11:56:14 +0100

    fix navigation

--- test_report/src/Nav.svelte
+++ test_report/src/Nav.svelte
@@ -16,7 +16,61 @@
 </script>
 
 <Tailwindcss />
-<div class="flex space-x-4 px-4 py-1 border-b items-center text-white" style="background-color: #525ebb;">
+<style>
+
+ #family-nav {
+
+    line-height: 50px;
+    float: right;
+    margin-top: 4px;
+    margin-bottom: 8px;
+ }
+
+ #project-nav {
+
+     line-height: 50px;
+     float: left;
+     margin-top: 8px;
+     margin-bottom: 8px;
+ }
+
+ #ig-status {
+
+     line-height: 20px;
+     margin-top: 17px;
+     text-align: center;
+     height: 50px;
+ }
+
+</style>
+
+<div id="segment-header" class="segment">
+    <div style="position:relative; height: 70px; padding: 0 3em;">
+        <div id="project-nav" style="float: left">
+            <a no-external="true" id="project-logo" href="index.html">
+                <img style="height: 50px;" src="http://sql-on-fhir.org/assets/images/sof-logo.svg" alt="SQL on FHIR" height="50">
+            </a>
+        </div>
+
+        <div id="family-nav">
+            <a no-external="true" id="family-logo" href="http://hl7.org/fhir">
+                <img style="height: 50px;"
+                     src="http://sql-on-fhir.org/assets/images/fhir-logo-www.png" alt="FHIR website">
+            </a>
+        </div>
+
+        <div id="ig-status">
+            <p><span style="font-size:12pt;font-weight:bold">SQL on FHIR</span>
+                <br>
+                <span style="display:inline-block;">2.0.0-pre - ci-build
+                    <img style="height: 16px; display: inline;" src="http://sql-on-fhir.org/assets/images/001.svg" alt="International flag" title="International">
+                </span>
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="flex space-x-4 border-b items-center text-white" style="background-color: #525ebb; padding: 0.5rem 3em;">
     {#each items as item}
         <a href="{item.href}" class="px-2 hover:text-white hover:bg-gray-150">{item.title}</a>
     {/each}
